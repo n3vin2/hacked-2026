e21e4609351e60451e0351c5ac3783e132580875
Nevin <nevinxuregina@gmail.com>
2026-02-21 21:47:26 -0700

    redid some servo properties, added some experimental features

--- sketch_feb21a/sketch_feb21a.ino
+++ sketch_feb21a/sketch_feb21a.ino
@@ -4,14 +4,12 @@ Servo myServoLeft;
 Servo myServoRight;
 
 int pos = 0;
+const int initial_pos = 110;
+const int angle = 40;
+
 int averageLight;
 
-void setup() {
-  // put your setup code here, to run once:
-  myServoLeft.attach(7);
-  myServoRight.attach(13);
-  Serial.begin(9600);
-
+void recalibrate() {
   int n = 1000;
   unsigned long lightSum = 0;
   for (int i = 0; i < n; i++) {
@@ -23,32 +21,69 @@ void setup() {
     Serial.print(" ");
     Serial.println(lightSum);
     delay(10);
-  }
-
+  } 
   averageLight = lightSum / n;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  myServoLeft.attach(A0);
+  myServoRight.attach(A1);
+  Serial.begin(9600);
+
+  //recalibrate();
   Serial.print("FINAL AVERAGE: ");
   Serial.println(averageLight);
-  //pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  int value = analogRead(A0);
-  Serial.println(value);
-  if (value < averageLight) {
-    for (pos = 0; pos <= 180; pos += 4) {
-      myServoLeft.write(pos);
-      myServoRight.write(pos);
-      delay(10);
-    }
-    delay(150);
-    for (pos = 180; pos >= 0; pos -= 4) {
-      myServoLeft.write(pos);
-      myServoRight.write(pos);
-      delay(10);
-    }
-    delay(250);
+  //Serial.println(value);
+  int pressed = digitalRead(8);
+  Serial.println(pressed);
+  if (pressed) {
+    //recalibrate();
   }
-  delay(250);
+  //Serial.println("reset");
+  //int value = analogRead(A0);
+  if (/* value < averageLight - 10 */1) {
+    //myServoLeft.write(180);
+    //Serial.println(INPUT);
+    //myServoLeft.write(0);
+    //digitalWrite(13, HIGH);
+    //delay(300);
+    //Serial.println("Writing");
+    for (int i = 0; i < 3; i++) {
+      for (pos = initial_pos; pos <= initial_pos + angle; pos++) {
+        myServoLeft.write(pos);
+        myServoRight.write(pos);
+        delay(10);
+      }
+      delay(150);
+      for (pos = initial_pos + angle; pos >= initial_pos; pos--) {
+        myServoLeft.write(pos);
+        myServoRight.write(pos);
+        delay(10);
+      }
+      /* for (pos = initial_pos; pos >= 15; pos--) {
+        myServoLeft.write(pos);
+        myServoRight.write(pos);
+        delay(10);
+      }
+      delay(150);
+      for (pos = 15; pos <= initial_pos; pos++) {
+        myServoLeft.write(pos);
+        myServoRight.write(pos);
+        delay(10);
+      } */
+      delay(250);
+    }
+  } else {
+    digitalWrite(13, LOW);
+    //delay(300);
+  }
+  delay(500);
 }
